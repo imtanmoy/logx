8c4c1906f90a5d30108cd58ae3e53c3eced007e8
Tanmoy Banik <imtanmoybanik@gmail.com>
2019-12-18 16:20:18 +0600

    comment added for linting

--- logx.go
+++ logx.go
@@ -42,66 +42,82 @@ func init() {
 	initialize()
 }
 
+// Debug logs a message at DebugLevel
 func Debug(args ...interface{}) {
 	defaultLogger.Debug(args...)
 }
 
+// Debugf logs a message at DebugLevel with string format
 func Debugf(format string, args ...interface{}) {
 	defaultLogger.Debugf(format, args...)
 }
 
+// Info logs a message at InfoLevel
 func Info(args ...interface{}) {
 	defaultLogger.Info(args...)
 }
 
+// Infof logs a message at InfoLevel with string format
 func Infof(format string, args ...interface{}) {
 	defaultLogger.Infof(format, args...)
 }
 
+// Warn logs a message at WarnLevel
 func Warn(args ...interface{}) {
 	defaultLogger.Warn(args...)
 }
 
+// Warnf logs a message at WarnLevel with string format
 func Warnf(format string, args ...interface{}) {
 	defaultLogger.Warnf(format, args...)
 }
 
+// Error logs a message at ErrorLevel
 func Error(args ...interface{}) {
 	defaultLogger.Error(args...)
 }
 
+// Errorf logs a message at ErrorLevel with string format
 func Errorf(format string, args ...interface{}) {
 	defaultLogger.Errorf(format, args...)
 }
 
+// Fatal logs a message at FatalLevel
 func Fatal(args ...interface{}) {
 	defaultLogger.Fatal(args...)
 }
 
+// Fatalf logs a message at FatalLevel with string format
 func Fatalf(format string, args ...interface{}) {
 	defaultLogger.Fatalf(format, args...)
 }
 
+// Panic logs a message at PanicLevel
 func Panic(args ...interface{}) {
 	defaultLogger.Panic(args...)
 }
 
+// Panicf logs a message at PanicLevel with string format
 func Panicf(format string, args ...interface{}) {
 	defaultLogger.Panicf(format, args...)
 }
 
+// Print logs a message at InfoLevel
 func Print(args ...interface{}) {
 	defaultLogger.Info(args...)
 }
 
+// Printf logs a message at InfoLevel with string format
 func Printf(format string, args ...interface{}) {
 	defaultLogger.Infof(format, args...)
 }
 
+// Println logs a message at InfoLevel
 func Println(args ...interface{}) {
 	defaultLogger.Info(args...)
 }
 
+// WithFields returns a new logger instance which can Log a message at the given level with context key/value pairs
 func WithFields(keyValues Fields) ILogger {
 	return defaultLogger.WithFields(keyValues)
 }
--- zap.go
+++ zap.go
@@ -45,66 +45,82 @@ type zapLogger struct {
 	sugaredLogger *zap.SugaredLogger
 }
 
+// Print logs a message at InfoLevel
 func (l *zapLogger) Print(args ...interface{}) {
 	l.sugaredLogger.Info(args...)
 }
 
+// Printf logs a message at InfoLevel with string format
 func (l *zapLogger) Printf(format string, args ...interface{}) {
 	l.sugaredLogger.Infof(format, args...)
 }
 
+// Println logs a message at InfoLevel
 func (l *zapLogger) Println(args ...interface{}) {
 	l.sugaredLogger.Info(args...)
 }
 
+// Debugf logs a message at DebugLevel with string format
 func (l *zapLogger) Debugf(format string, args ...interface{}) {
 	l.sugaredLogger.Debugf(format, args...)
 }
 
+// Debug logs a message at DebugLevel
 func (l *zapLogger) Debug(args ...interface{}) {
 	l.sugaredLogger.Debug(args...)
 }
 
+// Infof logs a message at InfoLevel with string format
 func (l *zapLogger) Infof(format string, args ...interface{}) {
 	l.sugaredLogger.Infof(format, args...)
 }
 
+// Info logs a message at InfoLevel
 func (l *zapLogger) Info(args ...interface{}) {
 	l.sugaredLogger.Info(args...)
 }
 
+// Warnf logs a message at WarnLevel with string format
 func (l *zapLogger) Warnf(format string, args ...interface{}) {
 	l.sugaredLogger.Warnf(format, args...)
 }
 
+// Warn logs a message at WarnLevel
 func (l *zapLogger) Warn(args ...interface{}) {
 	l.sugaredLogger.Warn(args...)
 }
 
+// Errorf logs a message at ErrorLevel with string format
 func (l *zapLogger) Errorf(format string, args ...interface{}) {
 	l.sugaredLogger.Errorf(format, args...)
 }
 
+// Error logs a message at ErrorLevel
 func (l *zapLogger) Error(args ...interface{}) {
 	l.sugaredLogger.Error(args...)
 }
 
+// Fatalf logs a message at FatalLevel with string format
 func (l *zapLogger) Fatalf(format string, args ...interface{}) {
 	l.sugaredLogger.Fatalf(format, args...)
 }
 
+// Fatal logs a message at FatalLevel
 func (l *zapLogger) Fatal(args ...interface{}) {
 	l.sugaredLogger.Fatal(args...)
 }
 
+// Panicf logs a message at PanicLevel with string format
 func (l *zapLogger) Panicf(format string, args ...interface{}) {
 	l.sugaredLogger.Fatalf(format, args...)
 }
 
+// Panic logs a message at PanicLevel
 func (l *zapLogger) Panic(args ...interface{}) {
 	l.sugaredLogger.Fatal(args...)
 }
 
+// WithFields returns a new logger instance which can Log a message at the given level with context key/value pairs
 func (l *zapLogger) WithFields(fields Fields) ILogger {
 	var f = make([]interface{}, 0)
 	for k, v := range fields {
